6590e5c4439f0af10552ae240540c9653e94e058
etiecher <eduardo.tiecher@gmail.com>
2015-10-25 18:46:34 -0200

    First update
    It includes the first public version of the script, readme file and tidy
    data resulted from the script.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # coursera
-Test and training repo for Data Scientist specialization on Coursera
+
+The run_analysis function was develop as an assignment to the Getting and Cleaning Data course
+by John Hopkins University on Coursera. It works over a dataset of measurements 
+of signals from the accelerometer and gyroscope of a Samsung Galaxy S smartphone carried by 30 different users
+performing six different activities during an experiment to analyze how these sensors would respond to these
+activities.
+
+Details on the dataset may be found at
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+Detailes on the assignment may be found at
+https://class.coursera.org/getdata-033/human_grading/view/courses/975117/assessments/3/submissions
+
+The function starts preparing the conditions to R perform the analysis. It opens dplyr library, download all the
+data and unzip it to local folders.
+
+The provided file strucutures divides the data into two datasets: one used for training and a second used to test
+the training data. For the purpose of the assignment, the script merges the two datasets into one called "complete"
+
+The assignment requires to analyze only the measurements
---
+++ run_analysis.R
@@ -0,0 +1,78 @@
+run_analysis <- function(){
+
+  # NEXT STEPS
+  # Guarantee that the final data is tidy
+  # Guarantee that the script access the files without any intervention outsite R
+
+  # This function was develop as an assignment to the Getting and Cleaning Data course
+  # by John Hopkins University on Coursera. It works over a dataset of measurements 
+  # of signals from the accelerometer and gyroscope of a Samsung Galaxy S smartphone.
+  # Details on the dataset may be found at
+  # http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+  # Detailes on the assignment may be found at
+  # https://class.coursera.org/getdata-033/human_grading/view/courses/975117/assessments/3/submissions
+
+  # PREPARATION
+  library(dplyr)
+  download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip","getdata.zip")
+  unzip("getdata.zip")
+    
+  # FIRST STEP
+  # Merge two datasets: test measuraments and train measurements.
+  test <- read.table("./UCI HAR Dataset/test/x_test.txt")
+  train <- read.table("./UCI HAR Dataset/train/x_train.txt")
+  #Merges the test and train data sets into one complete dataset
+  complete <- bind_rows(test,train)
+  
+  
+  # SECOND STEP
+  # Finds which columns from the data set contains the mean and standard deviation
+  # for each measurement. It is not clear from course instructions what is 
+  # considered "measurement". For the purpose of this exercise, I considered every
+  # variable with the texts "mean", "Mean" and "std" as variables to be selected.
+  
+  # Imports variables codes and names
+  features <- read.table("./UCI HAR Dataset/features.txt")
+  # Return the variables codes for those names containing the string "mean"
+  features_mean <- filter(features,grepl("mean",V2))
+  # Return the variables codes for those names containing the string "Mean"
+  features_Mean <- filter(features,grepl("Mean",V2))
+  # Return the variables codes for those names containing the string "std".
+  # Deserves note that exploring the database, no variable with "Std" was found.
+  features_std <- filter(features,grepl("std",V2))
+  # Binds all variables codes with "mean", "Mean" or "std" strings
+  features_selected <- bind_rows(features_mean,features_Mean,features_std)
+  
+  # THIRD STEP
+  # Include on the measurements datasets (train and test) the activity description
+  act_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+  train_labels_code <- read.table("./UCI HAR Dataset/train/y_train.txt")
+  test_labels_code <- read.table("./UCI HAR Dataset/test/y_test.txt")
+  train_labels <- select(merge(train_labels_code,act_labels),V2)
+  test_labels <- select(merge(test_labels_code,act_labels),V2)
+  labels <- bind_rows(test_labels,train_labels)
+  complete_act <- bind_cols(complete,labels)
+  
+  # FOURTH STEP
+  # Change the variable names on the dataset from codes to descriptive names
+  names(complete_act) <- c(paste(features$V2),"activity")
+  
+  
+  # FIFTH STEP
+  # Creates an independent tidy dataset with the average of each variable for 
+  # each activity and each subject
+  train_subjects <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+  test_subjects <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+  complete_subjects <- bind_rows(test_subjects,train_subjects)
+  
+  names(complete_subjects) <- "subject"
+  complete_act_sub <- bind_cols(complete_act,complete_subjects)
+  
+  names(complete_act_sub) <- make.names(names(complete_act_sub),unique = TRUE)
+
+  tidy_complete <- group_by(complete_act_sub,activity,subject)
+
+  summary <- summarise_each(tidy_complete, funs(mean))
+  write.table(summary,"summary.txt",row.names = FALSE)
+  print(summary)
+}
